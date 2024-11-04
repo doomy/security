49e332905b1f62687912dfdcab977713278f3a56
Vladimír Bártek <vladimir.bartek@gmail.com>
2024-11-04 21:52:01 +0100

    DummyAuthenticator

--- src/Authenticator/AuthenticatorInterface.php
+++ src/Authenticator/AuthenticatorInterface.php
@@ -4,17 +4,10 @@ declare(strict_types=1);
 
 namespace Doomy\Security\Authenticator;
 
-use Doomy\Security\LoginResult;
 use Doomy\Security\Model\User;
 
 interface AuthenticatorInterface
 {
-    /**
-     * @template T of User
-     * @param class-string<T> $userEntityClass
-     */
-    public function login(string $email, string $password, string $userEntityClass = User::class): LoginResult;
-
     /**
      * @template T of User
      * @param array<string, int|string> $headers
