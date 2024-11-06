8de3fb5180babe598b5bb80338c8412adc9650a3
Vladimír Bártek <vladimir.bartek@gmail.com>
2024-11-06 17:00:16 +0100

    Password strength validation uses czech dictionary

--- tests/PasswordServiceTest.php
+++ tests/PasswordServiceTest.php
@@ -11,8 +11,14 @@ final class PasswordServiceTest extends TestCase
     public function testWeakPassword(): void
     {
         $this->expectException(\Doomy\Security\Password\Exception\PasswordTooWeakException::class);
-
         $passwordService = new \Doomy\Security\PasswordService(new \ZxcvbnPhp\Zxcvbn());
         $passwordService->validatePasswordStrength('password');
+    }
+
+    public function testWeakPasswordCz(): void
+    {
+        $this->expectException(\Doomy\Security\Password\Exception\PasswordTooWeakException::class);
+        $passwordService = new \Doomy\Security\PasswordService(new \ZxcvbnPhp\Zxcvbn());
+        $passwordService->validatePasswordStrength('povazovan');
     }
 }
