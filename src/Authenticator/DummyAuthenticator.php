7ae522599290cd3fe73fed378c667cf2c9c31ebe
Vladimír Bártek <vladimir.bartek@gmail.com>
2024-11-25 12:39:31 +0100

    DummyAuthenticator can simulate expired and invalid token exception

--- src/Authenticator/DummyAuthenticator.php
+++ src/Authenticator/DummyAuthenticator.php
@@ -4,11 +4,25 @@ declare(strict_types=1);
 
 namespace Doomy\Security\Authenticator;
 
+use Doomy\Security\Exception\InvalidTokenException;
+use Doomy\Security\Exception\TokenExpiredException;
 use Doomy\Security\Model\User;
 
 final class DummyAuthenticator implements AuthenticatorInterface
 {
+    /**
+     * @param array<string, string> $headers
+     */
     public function authenticateRequest(array $headers, string $userEntityClass = User::class): void
     {
+        if (! array_key_exists('Authorization', $headers)) {
+            return;
+        }
+
+        if ($headers['Authorization'] === 'Bearer invalid') {
+            throw new InvalidTokenException();
+        } elseif ($headers['Authorization'] === 'Bearer expired') {
+            throw new TokenExpiredException();
+        }
     }
 }
