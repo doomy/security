4fe9ded7a5101cf78e75f78dbdffac12f80a55b0
Vladimír Bártek <vladimir.bartek@gmail.com>
2024-11-25 12:39:31 +0100

    DummyAuthenticator can simulate expired and invalid token exception

---
+++ tests/DummyAuthenticatorTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Doomy\Security\Tests;
+
+use Doomy\Security\Authenticator\DummyAuthenticator;
+use Doomy\Security\Exception\InvalidTokenException;
+use Doomy\Security\Exception\TokenExpiredException;
+use PHPUnit\Framework\TestCase;
+
+final class DummyAuthenticatorTest extends TestCase
+{
+    public function testOk(): void
+    {
+        $this->expectNotToPerformAssertions();
+        $dummyAuthenticator = new DummyAuthenticator();
+        $dummyAuthenticator->authenticateRequest([]);
+    }
+
+    public function testInvalidToken(): void
+    {
+        $this->expectException(InvalidTokenException::class);
+        $dummyAuthenticator = new DummyAuthenticator();
+        $dummyAuthenticator->authenticateRequest([
+            'Authorization' => 'Bearer invalid',
+        ]);
+    }
+
+    public function testExpiredToken(): void
+    {
+        $this->expectException(TokenExpiredException::class);
+        $dummyAuthenticator = new DummyAuthenticator();
+        $dummyAuthenticator->authenticateRequest([
+            'Authorization' => 'Bearer expired',
+        ]);
+    }
+}
